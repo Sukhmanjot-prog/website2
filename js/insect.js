3904da8d88a004200856452759a4a159493481a9
Sukhmanjot-prog <142855362+Sukhmanjot-prog@users.noreply.github.com>
2024-04-19 16:33:23 +0000

    Fri, Apr 19, 2024, 9:33 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -1,3 +1,26 @@
 const screens = document.querySelectorAll('.screen')
+const choose_insect_btns = document.querySelectorAll('.choose-insect-btns')
+const game_container = document.getElementById('game-container')
+const start_btn = document.getElementById('start-btn')
+const timeEl = document.getElementById('time')
+const scoreEl = document.getElementById('score')
+const message = document.getElementById('message')
+let seconds = 0
+let score = 0
+let selected_insect = {}
 
-screens[0].classList.add('up')
+console.log(screens)
+start_btn.addEventListener('click', () => {
+    screens[0].classList.add('up')
+})
+
+choose_insect_btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const img = btn.querySelector('img')
+        const alt = img.getAttribute('alt')
+        const src = img.getAttribute('src')
+        screens[1].classList.add('up')
+        selected_insect = {src, alt}
+        setTimeout(createInsect, 1000)
+    })
+})
